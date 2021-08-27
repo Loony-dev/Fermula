8342eb45f90aa600dc62309481e471160f7c884b
Loony-dev <viktorastapov25@gmail.com>
2021-08-27 15:24:47 +0300

    update

--- src/panels/Intro/Intro.jsx
+++ src/panels/Intro/Intro.jsx
@@ -2,27 +2,14 @@ import React, {Fragment} from 'react';
 
 import {Panel, PanelHeader, Avatar, Group, Div, Header} from '@vkontakte/vkui';
 
-import Coin from '../../img/coin.png'
-import Lightning from '../../img/lightning.png'
-
 import './Intro.css';
 
 const Intro = ({id, snackbarError, fetchedUser, userHasSeenIntro, go}) => {
     return (
         <Panel id={id} separator={false}>
-            <Header className="header" />
+            <Header className="header-padding" />
             <nav id="navbar" className="navbar">
                 <div className="navbar-logo">Фермула</div>
-                <div className="navbar-info">
-                    <div className="navbar-balance">
-                        <img src={Coin} alt="coin" />
-                        <span>350</span>
-                    </div>
-                    <div className="navbar-energy">
-                        <img src={Lightning} alt="lightning" />
-                        <span>350</span>
-                    </div>
-                </div>
             </nav>
 
             {/*<PanelHeader>
@@ -38,6 +25,37 @@ const Intro = ({id, snackbarError, fetchedUser, userHasSeenIntro, go}) => {
                     </Group>
                 </Fragment>
             }*/}
+
+            <header id="header" className="header">
+                <div className="welcome-text">
+                    <h3>Фермула | построй свой завод</h3>
+                    <p>Cтрой заводы, прокачивай свой город <br /> и поборись за первое место в топе</p>
+                </div>
+            </header>
+
+            <main id="main" className="main">
+                <div className="row">
+                    <div className="col-6">
+                        <button className="buttons-primary w-100"><span>Начать игру</span></button>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <p className="text-center text-desc mt mb">
+                        Построй свою империю по производству автомобилей! присоединяйся к <span>345</span> игрокам уже
+                        сегодня!
+                    </p>
+                </div>
+
+                <div className="row">
+                    <div className="welcome-content">
+                        <h3>Что за фермула?</h3>
+                        <p>Игра, становящаяся все лучше и лучше с каждым днем! Игра, от которой не так то просто
+                            оторваться! Хочешь почувствовать себя настоящим гонщиком? Тогда регистрируйся, приглашай
+                            друзей, покупай и прокачивай тачки, выигрывай турниры и стань чемпионом!</p>
+                    </div>
+                </div>
+            </main>
 
             {snackbarError}
         </Panel>
