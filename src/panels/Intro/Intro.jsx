a1b9d2c69ffb32c346ec7ec875581c8ad30f8af6
Loony-dev <viktorastapov25@gmail.com>
2021-08-27 13:54:09 +0300

    update 3

--- src/panels/Intro/Intro.jsx
+++ src/panels/Intro/Intro.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import { Panel, PanelHeader } from '@vkontakte/vkui';
+import {Panel, PanelHeader} from '@vkontakte/vkui';
 
 import './Intro.css';
 
-const Intro = (id, snackbarError, fetchedUser, userHasSeenIntro, go) => {
-	return (
-		<Panel id={id} centered={true}>
-			<PanelHeader>
-				Фермула
-			</PanelHeader>
+const Intro = ({id, snackbarError, fetchedUser, userHasSeenIntro, go}) => {
+    return (
+        <Panel id={id} centered={true}>
+            <PanelHeader>
+                Фермула
+            </PanelHeader>
 
-			{(userHasSeenIntro && fetchedUser) && 'Hi!'}
+            {(userHasSeenIntro && fetchedUser) && 'Hi!'}
 
-			{snackbarError}
-		</Panel>
-	)
+            {snackbarError}
+        </Panel>
+    )
 };
 
 export default Intro;
